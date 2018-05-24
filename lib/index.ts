1c78afeb7b4b66782720446d1a1155601ad7b682
Cody Dalton <codyjdalton@gmail.com>
2018-05-24 05:52:07 -0700

    0.3.1: Update documentation and export metadata

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 // export your main module:
 export { Injectable } from './decorators/injectable.decorator';
 export { Injector } from './modules/injector.module';
+export { Metadata } from './decorators/metadata.decorator';
